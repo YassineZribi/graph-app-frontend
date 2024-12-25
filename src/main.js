24c603a0cc7b8b304395116c02c5558f8d3c7ced
Yassine Zribi <yassinezribiyassine@gmail.com>
2024-12-25 22:46:32 +0100

    Set up the application

--- src/main.js
+++ src/main.js
@@ -1,24 +1,489 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'notyf/notyf.min.css'
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import { DataSet, Network } from 'vis-network/standalone'
+import { Notyf } from 'notyf'
+import axios from 'axios'
+import * as bootstrap from 'bootstrap'
+import { formatPath, getTotalPathDistance } from './utils'
 
-setupCounter(document.querySelector('#counter'))
+
+const nodes = new DataSet();
+const edges = new DataSet();
+
+let selectedNodeId = null;
+let selectedEdgeId = null;
+let lastClickPosition = { x: 0, y: 0 };
+
+const NAVBAR_HEIGHT = parseInt(getComputedStyle(document.getElementsByClassName("navbar")?.[0])?.height)
+
+// Dijekstra
+let startNode = null;
+let endNode = null;
+
+var notyf = new Notyf({
+  dismissible: true
+});
+
+const container = document.getElementById('network');
+const data = { nodes, edges };
+const options = { /* physics: false, interaction: { dragNodes: true }, */ edges: { smooth: true } };
+const network = new Network(container, data, options);
+
+
+/*
+network.on("beforeDrawing", function (ctx) {
+
+    var width = ctx.canvas.clientWidth;
+    var height = ctx.canvas.clientHeight;
+    var spacing = 20;
+    var gridExtentFactor = 4;
+    ctx.strokeStyle = "lightgrey";
+
+    // draw grid
+    ctx.beginPath();
+    for (var x = -width * gridExtentFactor; x <= width * gridExtentFactor; x += spacing) {
+        ctx.moveTo(x, height * gridExtentFactor);
+        ctx.lineTo(x, -height * gridExtentFactor);
+    }
+    for (var y = -height * gridExtentFactor; y <= height * gridExtentFactor; y += spacing) {
+        ctx.moveTo(width * gridExtentFactor, y);
+        ctx.lineTo(-width * gridExtentFactor, y);
+    }
+    ctx.stroke();
+
+});
+*/
+
+network.on("beforeDrawing", function (ctx) {
+  var width = ctx.canvas.clientWidth;
+  var height = ctx.canvas.clientHeight;
+  var spacing = 20;
+  var gridExtentFactor = 4;
+  ctx.fillStyle = "lightgrey";
+
+  // Dessiner une grille de points
+  for (var x = -width * gridExtentFactor; x <= width * gridExtentFactor; x += spacing) {
+    for (var y = -height * gridExtentFactor; y <= height * gridExtentFactor; y += spacing) {
+      ctx.beginPath();
+      ctx.arc(x, y, 1.25, 0, 2 * Math.PI); // Dessiner un petit point (rayon 1.25)
+      ctx.fill();
+    }
+  }
+});
+
+
+// ✅ Gestion du Double-Clic
+network.on('doubleClick', (params) => {
+  console.log(params)
+  console.log(params.nodes)
+  console.log(params.edges)
+
+  if (params.nodes.length == 0 && params.edges.length == 0) { // Clicked on an empty area
+    lastClickPosition = params.pointer.canvas;
+    const addPopover = document.getElementById('addPopover');
+    addPopover.style.left = `${params.pointer.DOM.x}px`;
+    addPopover.style.top = `${params.pointer.DOM.y + NAVBAR_HEIGHT}px`;
+    addPopover.style.display = 'block';
+    document.getElementById('addNodeLabel').focus()
+  } else if (params.nodes.length == 0 && params.edges.length > 0) { // Clicked on an edge
+    // ✅ Gérer le Double-Clic sur un Lien (Edge)
+    selectedEdgeId = params.edges[0];
+    const edge = edges.get(selectedEdgeId);
+    const position = params.pointer.DOM;
+
+    const editEdgePopover = document.getElementById('editEdgePopover');
+    editEdgePopover.style.left = `${position.x}px`;
+    editEdgePopover.style.top = `${position.y + NAVBAR_HEIGHT}px`;
+    editEdgePopover.style.display = 'block';
+
+    document.getElementById('editEdgeLabel').value = edge.label || '';
+  } else { // Clicked on a node (params.nodes.length > 0)
+    selectedNodeId = params.nodes[0];
+    const position = network.getPositions([selectedNodeId])[selectedNodeId];
+    const canvasPosition = network.canvasToDOM(position);
+
+    document.getElementById('editNodePopover').style.left = `${canvasPosition.x}px`;
+    document.getElementById('editNodePopover').style.top = `${canvasPosition.y + NAVBAR_HEIGHT}px`;
+    document.getElementById('editNodePopover').style.display = 'block';
+    document.getElementById('editNodeLabel').value = nodes.get(selectedNodeId).label;
+    populateLinkNodeSelect(selectedNodeId);
+  }
+});
+
+// ✅ Modifier le Label du Lien
+document.getElementById('editEdgeBtn').addEventListener('click', () => {
+  const newLabel = document.getElementById('editEdgeLabel').value;
+  edges.update({ id: selectedEdgeId, label: newLabel });
+  closeEditEdgePopover();
+});
+
+// ✅ Afficher le Modal de Confirmation avant la Suppression
+document.getElementById('deleteEdgeBtn').addEventListener('click', () => {
+  const confirmModal = new bootstrap.Modal(document.getElementById('confirmEdgeDeleteModal'));
+  confirmModal.show();
+
+  closeEditEdgePopover();
+});
+
+// ✅ Confirmer et Supprimer le Lien
+document.getElementById('confirmDeleteEdgeBtn').addEventListener('click', () => {
+  if (selectedEdgeId) {
+    edges.remove(selectedEdgeId);
+    closeModalById('confirmEdgeDeleteModal')
+  }
+});
+
+// ✅ Ajouter un Nœud
+document.getElementById('addNodeBtn').addEventListener('click', () => {
+  const label = document.getElementById('addNodeLabel').value.trim();
+  if (label.length > 2 || label === '' || nodes.get({ filter: (node) => node.label === label }).length > 0) {
+    notyf.error('Label invalide ou déjà existant.');
+    return;
+  }
+  nodes.add({ label, x: lastClickPosition.x, y: lastClickPosition.y });
+  closeAddNodePopover();
+});
+
+// ✅ Modifier un Nœud
+document.getElementById('editNodeBtn').addEventListener('click', () => {
+  const label = document.getElementById('editNodeLabel').value;
+  if (label.length > 2 || label === '' || nodes.get({ filter: (node) => node.label === label }).length > 0) {
+    notyf.error('Label invalide ou déjà existant.');
+    return;
+  }
+  // Mettre à jour le nœud
+  nodes.update({ id: selectedNodeId, label });
+
+  // Mettre à jour les arêtes liées au nœud modifié
+  const connectedEdges = edges.get().filter(edge => edge.from === selectedNodeId || edge.to === selectedNodeId);
+  connectedEdges.forEach(edge => {
+    if (edge.from === selectedNodeId) {
+      edge.from = selectedNodeId; // ID reste le même, mais on peut ajouter des mises à jour si nécessaire
+    }
+    if (edge.to === selectedNodeId) {
+      edge.to = selectedNodeId; // ID reste le même, mais on peut ajouter des mises à jour si nécessaire
+    }
+    edges.update(edge);
+  });
+
+  closeEditNodePopover()
+  console.log(nodes.get());
+  console.log(edges.get());
+});
+
+// ✅ Supprimer un Nœud
+document.getElementById('deleteNodeBtn').addEventListener('click', () => {
+  const confirmModal = new bootstrap.Modal('#confirmDeleteNodeModal');
+  confirmModal.show();
+
+  closeEditNodePopover();
+});
+
+// ✅ Supprimer un Graphe
+document.getElementById('deleteGraphBtn').addEventListener('click', () => {
+  if (nodes.get().length == 0) {
+    alert('No graph to delete!');
+    return;
+  }
+  const confirmModal = new bootstrap.Modal('#confirmDeleteGraphModal');
+  confirmModal.show();
+});
+
+document.getElementById('confirmDeleteGraphBtn').addEventListener('click', () => {
+  deleteGraph()
+});
+
+document.getElementById('confirmDeleteNodeBtn').addEventListener('click', () => {
+  if (selectedNodeId) {
+    // Supprimer le noeud
+    nodes.remove(selectedNodeId);
+
+    // Supprimer les arêtes liées au noeud
+    const edgesToRemove = edges.get().filter(edge => edge.from === selectedNodeId || edge.to === selectedNodeId);
+    edgesToRemove.forEach(edge => edges.remove(edge.id));
+
+    closeModalById('confirmDeleteNodeModal')
+
+    console.log(nodes.get())
+    console.log(edges.get())
+  }
+});
+
+// ✅ Lier Deux Nœuds
+document.getElementById('createEdgeBtn').addEventListener('click', () => {
+  const targetNode = document.getElementById('linkNodeSelect').value;
+  const label = document.getElementById('edgeLabel').value;
+  const isDirected = document.getElementById('isDirectedEdge').checked;
+
+  if (!targetNode || label === '') {
+    alert('Veuillez sélectionner un nœud et entrer un label valide pour l’arête.');
+    return;
+  }
+
+  edges.add({ from: selectedNodeId, to: targetNode, label, arrows: isDirected ? 'to' : '', /*color: { color: 'green' }*/ });
+  console.log(edges.get());
+
+  closeEditNodePopover()
+});
+
+// ✅ Fermer les popovers en cliquant à l'extérieur
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.popover') && !e.target.closest('.vis-network')) {
+    closeEditNodePopover();
+    closeEditEdgePopover();
+    closeAddNodePopover();
+  }
+});
+
+// ✅ Mettre à jour les sélecteurs lorsqu'ils sont ouverts
+function updateNodeSelectOptions(selectElement) {
+  selectElement.innerHTML = '<option value="">-</option>';
+
+  nodes.forEach((node) => {
+    const option = document.createElement('option');
+    option.value = node.id;
+    option.text = node.label;
+    selectElement.appendChild(option);
+  });
+}
+
+// ✅ Écouter l'ouverture des sélecteurs
+document.getElementById('startNodeSelect').addEventListener('focus', () => {
+  updateNodeSelectOptions(document.getElementById('startNodeSelect'));
+});
+
+document.getElementById('endNodeSelect').addEventListener('focus', () => {
+  updateNodeSelectOptions(document.getElementById('endNodeSelect'));
+});
+
+// ✅ Mettre à jour les variables lors du changement des sélecteurs
+document.getElementById('startNodeSelect').addEventListener('change', (e) => {
+  startNode = e.target.value || null;
+});
+
+document.getElementById('endNodeSelect').addEventListener('change', (e) => {
+  endNode = e.target.value || null;
+});
+
+// ✅ Afficher les variables dans la console
+document.getElementById('calculateDijekstraBtn').addEventListener('click', () => {
+  console.log('Nœud de Départ:', startNode);
+  console.log('Nœud d\'Arrivée:', endNode);
+
+  const API_URL = 'http://127.0.0.1:5000/api/dijekstra';
+
+  const data = {
+    graph: {
+      nodes: nodes.get(),
+      edges: edges.get()
+    },
+    startNode,
+    endNode
+  }
+
+  if (data.graph.nodes.length == 0 || !startNode || !endNode) {
+    notyf.error("Can't perform Dijekstra algorithm!")
+    return
+  }
+  resetEdgesStyle();
+  axios.post(API_URL, data)
+    .then(res => {
+      console.log('Success:', res.data);
+      const selectedEdges = res.data.selectedEdges
+      const selectedNodes = res.data.selectedNodes
+      console.log(selectedNodes)
+      document.getElementById("dijekstra-path").innerHTML = formatPath(selectedNodes)
+      document.getElementById("dijekstra-distance").innerHTML = `<b>Total:</b> <span>${getTotalPathDistance(selectedEdges)}</span>`
+      network.selectEdges([])
+      network.selectNodes([])
+      selectedEdges.forEach(edge => edges.update({ id: edge.id, color: { color: "#06D6A0"}, width: 3 }))
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
+
+// ✅ Réinitialiser les sélecteurs et les variables avec le bouton Vider
+document.getElementById('resetDijkstraBtn').addEventListener('click', () => {
+  document.getElementById('startNodeSelect').value = '';
+  document.getElementById('endNodeSelect').value = '';
+  startNode = null;
+  endNode = null;
+  resetEdgesStyle()
+  document.getElementById("dijekstra-path").innerHTML = ""
+  document.getElementById("dijekstra-distance").innerHTML = ""
+});
+
+// ✅ Fermer simplement l'accordion avec le bouton Fermer
+document.getElementById('closeDijkstraAccordionBtn').addEventListener('click', () => {
+  const accordion = new bootstrap.Collapse('#collapseDijkstra');
+  accordion.hide();
+});
+
+// ✅ Remplit le select avec les autres nœuds disponibles pour la liaison
+function populateLinkNodeSelect(currentNodeId) {
+  const select = document.getElementById('linkNodeSelect');
+  select.innerHTML = '<option value="">-</option>'; // Réinitialiser le select
+
+  nodes.forEach((node) => {
+    if (node.id !== currentNodeId) {
+      // Vérifier si une arête existe déjà entre les deux nœuds
+      const edgeExists = edges.get({
+        filter: (edge) => edge.arrows
+          ? (edge.from === currentNodeId && edge.to === node.id)
+          : ((edge.from === currentNodeId && edge.to === node.id) || (edge.to === currentNodeId && edge.from === node.id))
+      }).length > 0;
+
+      const option = document.createElement('option');
+      option.value = node.id;
+      option.text = node.label;
+      option.disabled = edgeExists; // Désactiver si un lien existe déjà
+
+      select.appendChild(option);
+    }
+  });
+
+  // Afficher les détails du lien après sélection
+  select.addEventListener('change', () => {
+    const linkDetails = document.getElementById('linkDetails');
+    if (select.value) {
+      openLinkDetailsBlock()
+    } else {
+      closeLinkDetailsBlock()
+    }
+  });
+}
+
+function closeEditNodePopover() {
+  document.getElementById('editNodePopover').style.display = 'none';
+
+  closeLinkDetailsBlock()
+}
+
+document.getElementById('closeEditNodePopoverBtn').addEventListener('click', closeEditNodePopover);
+
+
+function closeEditEdgePopover() {
+  document.getElementById('editEdgePopover').style.display = 'none';
+}
+
+document.getElementById('closeEditEdgePopoverBtn').addEventListener('click', closeEditEdgePopover);
+
+
+function closeAddNodePopover() {
+  document.getElementById('addPopover').style.display = 'none';
+
+  document.getElementById('addNodeLabel').value = ""
+}
+
+document.getElementById('closeAddNodePopoverBtn').addEventListener('click', closeAddNodePopover);
+
+
+function openLinkDetailsBlock() {
+  linkDetails.style.display = 'block';
+}
+function closeLinkDetailsBlock() {
+  linkDetails.style.display = 'none';
+
+  document.getElementById('edgeLabel').value = ""
+  document.getElementById("isDirectedEdge").checked = false;
+}
+
+function closeModalById(modalId) {
+  const modal = bootstrap.Modal.getInstance(document.getElementById(modalId));
+  modal.hide();
+}
+
+function resetEdgesStyle() {
+  edges.get().forEach(edge => edges.update({ id: edge.id, color: {}, width: 1 }))
+}
+
+function showData() {
+  const data = {
+    nodes: nodes.get(),
+    edges: edges.get()
+  }
+  console.log(JSON.stringify(data, null, 2))
+}
+
+document.getElementById('showLocalDataBtn').addEventListener('click', showData);
+
+function saveData() {
+  const API_URL = 'http://127.0.0.1:5000/api/graph'; // Replace with your REST API endpoint
+  const data = {
+    nodes: nodes.get(),
+    edges: edges.get()
+  }
+
+  if (data.nodes.length == 0) {
+    notyf.error("No graph available to save!")
+    return
+  }
+
+  axios.post(API_URL, data)
+    .then(res => {
+      console.log('Success:', res.data);
+      notyf.success("Graph saved successfully!")
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+document.getElementById('saveDataBtn').addEventListener('click', saveData);
+
+function deleteGraph() {
+  const API_URL = 'http://127.0.0.1:5000/api/graph'; // Replace with your REST API endpoint
+  axios.delete(API_URL)
+    .then(res => {
+      console.log('Success:', res);
+      closeModalById('confirmDeleteGraphModal')
+      notyf.success("Graph deleted successfully!")
+      nodes.clear();
+      edges.clear();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function loadGraph() {
+  const API_URL = 'http://127.0.0.1:5000/api/graph'; // Replace with your REST API endpoint
+  axios.get(API_URL)
+    .then(res => {
+      console.log('Success:', res.data);
+      nodes.clear();
+      edges.clear();
+      res.data.nodes.forEach(node => nodes.add({ id: node.id, label: node.label }))
+      res.data.edges.forEach(edge => edges.add({ ...edge }))
+      // nodes.add({ label, x: lastClickPosition.x, y: lastClickPosition.y });
+      // edges.add({ from: selectedNodeId, to: targetNode, label, arrows: isDirected ? 'to' : '' });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      notyf.error(error.response.data.msg)
+    });
+}
+
+document.getElementById('loadGraphBtn').addEventListener('click', loadGraph);
+
+function transformGraph() {
+  const allNodes = nodes.get()
+  const allEdges = edges.get()
+  const transformedGraph = {}
+  allNodes.forEach(node => {
+    neighbors = {}
+    allEdges.forEach(edge => {
+      if ((node.id == edge.from && edge.arrows == "to") || ((node.id == edge.from || node.id == edge.to) && edge.arrows != "to")) {
+        neighbors[edge.to != node.id ? (allNodes.find(n => n.id == edge.to)?.label) : (allNodes.find(n => n.id == edge.from)?.label)] = Number(edge.label)
+      }
+    })
+    transformedGraph[node.label] = neighbors
+  })
+  console.log(transformedGraph)
+}
+
+document.getElementById('transformGraphBtn').addEventListener('click', transformGraph);
---
+++ src/utils.js
@@ -0,0 +1,12 @@
+export function getTotalPathDistance(selectedEdges = []) {
+    let total = 0
+    selectedEdges.forEach(edge => total += Number(edge.label))
+    return total ? total : "-"
+}
+
+export function formatPath(nodes) {
+    if (!nodes || nodes.length === 0) {
+        return '-';
+    }
+    return nodes.map(node => `<b>${node.label}</b>`).join('<span>&#8594;</span>');
+}
