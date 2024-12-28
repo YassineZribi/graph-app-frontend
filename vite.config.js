91599b93fa91cf1a67b10dde5e9a3d8d29860c08
Yassine Zribi <yassinezribiyassine@gmail.com>
2024-12-28 01:15:07 +0100

    Add 404.html page configuration

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login/index.html'),
+        notFound: resolve(__dirname, '404.html'),
       },
     },
   },
